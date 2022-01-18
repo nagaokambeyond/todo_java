d5c7b6205b6487cffa03219f35048844ab95bfee
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2022-01-19 00:09:16 +0900

    リファクタ

--- demo/src/main/java/com/example/demo/controller/ToDoController.java
+++ demo/src/main/java/com/example/demo/controller/ToDoController.java
@@ -1,38 +1,32 @@
 package com.example.demo.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import com.example.demo.repository.ToDoRepository;
-import com.example.demo.entity.ToDo;
 import com.example.demo.response.ToDoResponse;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ToDoController {
     @Autowired
     ToDoRepository repository;
 
+    @Autowired
+    ModelMapper map;
+
     @GetMapping("/todo")
     public List<ToDoResponse> getToDoList(){
-        var aaa = repository.findAll();
         var result = new ArrayList<ToDoResponse>();
-        ModelMapper x = new ModelMapper();
-        for(var kk : aaa){
-            var pp = x.map(kk, ToDoResponse.class);
-            /*
-            ToDoResponse pp = new ToDoResponse();
-            pp.setId(kk.getId());
-            pp.setStatus(kk.getStatus());
-            pp.setMessage(kk.getMessage());
-            */
-            result.add(pp);
-        }        
+        for(var row : repository.findAll()){
+            var todo = map.map(row, ToDoResponse.class);
+            result.add(todo);
+        }
 
         return result;
     }
--- demo/src/main/java/com/example/demo/entity/ToDo.java
+++ demo/src/main/java/com/example/demo/entity/ToDo.java
@@ -2,11 +2,8 @@ package com.example.demo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.TableGenerator;
 
 import lombok.Data;
 
@@ -15,21 +12,6 @@ import lombok.Data;
 @Table(name="todo")
 public class ToDo {
     @Id
-    //@GeneratedValue(strategy = GenerationType.AUTO)
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
-    //@Column
-    /*
-    @GeneratedValue(generator = "todo_comments")  // Generator 名 (何でもよい)
-    @TableGenerator(
-            name = "todo_comments",  // @GeneratedValue.generator と合わせる
-            table = "sqlite_sequence",  // SQLite のシーケンステーブル名と合わせる
-            pkColumnName = "name",  // sqlite_sequence のシーケンスカラム名 (name 固定)
-            valueColumnName = "seq",  // sqlite_sequence のシーケンス値名 (seq 固定)
-            pkColumnValue = "comments",  // sqlite_sequence.name に格納されている値 (テーブル名)
-            initialValue = 1,  // シーケンス初期値. 多くの場合 1
-            allocationSize = 1  // AUTO INCREMENT される場合の増減値. 何故かデフォルト 50 になっているので 1 を指定する
-    )    
-    */
     private Long id;
 
     @Column(nullable = false)
@@ -38,34 +20,4 @@ public class ToDo {
     @Column(nullable = false)
     private String message;
 
-    /*
-public ToDo(Long status, String message){
-    this.status = status;
-    this.message = message;
-}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getStatus(){
-        return status;
-    }
-
-    public void setStatus(Long status){
-        this.status = status;
-    }
-
-    public String getMessage(){
-        return message;
-    }
-
-    public void setMessage(String message){
-        this.message = message;
-    }
-    */
 }
