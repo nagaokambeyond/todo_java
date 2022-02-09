1858833ab7b9825aed77050941d0705d9b265359
nagaokambeyond <40172017+nagaokambeyond@users.noreply.github.com>
2022-02-10 02:03:25 +0900

    リファクタリング

--- demo/src/main/java/com/example/demo/api/ToDosRestController.java
+++ demo/src/main/java/com/example/demo/api/ToDosRestController.java
@@ -13,17 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.BindException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import lombok.NonNull;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api/v1/todos")
@@ -32,20 +34,26 @@ public class ToDosRestController {
   @Autowired
   protected ToDoService service;
 
+	@ExceptionHandler(BindException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<Object> handleBindException(BindException bindException, Locale locale) {
+    List<ValidationErrorResponse> errors = bindException.getFieldErrors().stream()
+      .map(r -> {
+        var row = new ValidationErrorResponse();
+        row.setFieldName(r.getField());
+        row.setMessage(r.getDefaultMessage());
+        return row;
+      }).collect(Collectors.toList());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+	}
+
   @PutMapping
   public ResponseEntity<Object> done(
     @RequestBody @Validated ToDoDoneRequest request,
     BindingResult result
-  ) {
+  ) throws BindException {
     if (result.hasErrors()) {
-      List<ValidationErrorResponse> errors = result.getFieldErrors().stream()
-        .map(r -> {
-          var row = new ValidationErrorResponse();
-          row.setFieldName(r.getField());
-          row.setMessage(r.getDefaultMessage());
-          return row;
-      }).collect(Collectors.toList());
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+      throw new BindException(result);
     }
     service.done(request);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
@@ -63,16 +71,9 @@ public class ToDosRestController {
   public ResponseEntity<Object> save(
     @RequestBody @Validated ToDoCreateRequest request,
     BindingResult result
-  ) {
+  ) throws BindException {
     if (result.hasErrors()) {
-      List<ValidationErrorResponse> errors = result.getFieldErrors().stream()
-        .map(r -> {
-          var row = new ValidationErrorResponse();
-          row.setFieldName(r.getField());
-          row.setMessage(r.getDefaultMessage());
-          return row;
-      }).collect(Collectors.toList());
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+      throw new BindException(result);
     }
     service.save(request);
     return ResponseEntity.status(HttpStatus.CREATED).body(null);
